caf007dc602ed78f2a0f1a4feeffff6e16cca95b
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-03-21 00:21:28 +0300

    feat: configure ApiUrl & singup submit

---
+++ src/assets/signup.jsx
@@ -0,0 +1,50 @@
+
+  
+  export const mainContentStyle = {
+    width: "60%",
+    borderRadius: "20px",
+    boxShadow: "10px 10px 10px 10px rgba(0, 0, 0, 0.4)",
+    margin: "5em auto",
+    display: "flex",
+  };
+  
+  export const companyInfoStyle = {
+    backgroundColor: "#008080",
+    borderTopLeftRadius: "20px",
+    borderBottomLeftRadius: "20px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    color: "#fff",
+    padding: "3em",
+  };
+  
+  export const loginFormStyle = {
+    backgroundColor: "#fff",
+    width: "100%",
+    marginLeft: "2em",
+  };
+  
+  export const inputStyle = {
+    width: "75%",
+    border: "0px solid transparent",
+    borderRadius: "0",
+    borderBottom: "1px solid #aaa",
+    padding: "1em 0.5em 0.5em",
+    paddingLeft: "2em",
+    outline: "none",
+    margin: "15px auto",
+    transition: "all 0.5s ease",
+  };
+  
+  export const submitButtonStyle = {
+    transition: "all 0.5s ease",
+  width: "70%",
+  borderRadius: "30px",
+  color: "#008080",
+  fontWeight: "600",
+  backgroundColor: "#fff",
+  border: "1px solid #008080",
+  marginTop: "1.5em",
+  marginBottom: "1em",
+  };
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,158 +1,96 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { apiUrl } from '../config/config';
+import { mainContentStyle, companyInfoStyle, loginFormStyle, inputStyle, submitButtonStyle } from '../assets/signup';
 
 function SignupPage() {
+    const [user, setUser] = useState({
+        firstname: '',
+        lastname: '',
+        birthday: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser({
+            ...user,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: user.firstname,
+                    username: user.lastname,
+                    birthday: user.birthday,
+                    email: user.email,
+                    password: user.password,
+                }),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                alert('User registered successfully');
+            } else {
+                alert('Failed to register user');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Error submitting form');
+        }
+    };
+
     return (
-      <>
-        <div className="container-fluid">
-          <div style={mainContentStyle}>
-            <div className="company__info" style={companyInfoStyle}>
-              <span className="company__logo">
-                <h2>
-                  <span
-                    className="fa fa-android fa fa-lock"
-                    style={{ fontSize: "3em" }}
-                  ></span>
-                </h2>
-              </span>
-              <h4 className="company_title">Notre Logo ici</h4>
+        <form onSubmit={handleSubmit}>
+            <div className="container-fluid">
+                <div style={mainContentStyle}>
+                    <div className="company__info" style={companyInfoStyle}>
+                        <span className="company__logo">
+                            <h2>
+                                <span className="fa fa-android fa fa-lock" style={{ fontSize: "3em" }}></span>
+                            </h2>
+                        </span>
+                        <h4 className="company_title">Notre Logo ici</h4>
+                    </div>
+                    <div className="login_form" style={loginFormStyle}>
+                        <div style={{ padding: "0 2em" }}>
+                            <div style={{ color: "#008080" }}>
+                                <h2>Sign Up</h2>
+                            </div>
+                            <div style={{ display: "flex", flexWrap: "wrap" }}>
+                                <div style={{ flex: "1", marginRight: "10px" }}>
+                                    <input type="text" name="firstname" className="form__input" placeholder="Name" style={inputStyle} value={user.firstname} onChange={handleChange} />
+                                    <input type="text" name="lastname" className="form__input" placeholder="Username" style={inputStyle} value={user.lastname} onChange={handleChange} />
+                                    <input type="text" name="birthday" className="form__input" placeholder="Birthday" style={inputStyle} value={user.birthday} onChange={handleChange} />
+                                    <input type="text" name="email" className="form__input" placeholder="Email" style={inputStyle} value={user.email} onChange={handleChange} />
+                                </div>
+                                <div style={{ flex: "1", marginLeft: "10px" }}>
+                                    <input type="password" name="password" className="form__input" placeholder="Password" style={inputStyle} value={user.password} onChange={handleChange} />
+                                </div>
+                            </div>
+                            <div>
+                                <input type="submit" value="Submit" className="btn" style={submitButtonStyle} />
+                            </div>
+                        </div>
+                        <div>
+                            <p>
+                                Do you already have an account? <Link to="/">Log In</Link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-  
-            <div className="login_form" style={loginFormStyle}>
-              <div style={{ padding: "0 2em" }}>
-                <div style={{ color: "#008080" }}>
-                  <h2>Sign Up</h2>
-                </div>
-                <div style={{ display: "flex", flexWrap: "wrap" }}>
-                  <div style={{ flex: "1", marginRight: "10px" }}>
-                    <input
-                      type="text"
-                      name="Firstname"
-                      id="firstname"
-                      className="form__input"
-                      placeholder="Firstname"
-                      style={inputStyle}
-                    />
-                    <input
-                      type="text"
-                      name="Lastname"
-                      id="lastname"
-                      className="form__input"
-                      placeholder="Lastname"
-                      style={inputStyle}
-                    />
-                    <input
-                      type="text"
-                      name="Birthday"
-                      id="birthday"
-                      className="form__input"
-                      placeholder="Birthday"
-                      style={inputStyle}
-                    />
-                    <input
-                      type="text"
-                      name="Email"
-                      id="email"
-                      className="form__input"
-                      placeholder="Email"
-                      style={inputStyle}
-                    />
-                  </div>
-                  <div style={{ flex: "1", marginLeft: "10px" }}>
-                    <input
-                      type="text"
-                      name="Contact"
-                      id="contact"
-                      className="form__input"
-                      placeholder="Contact"
-                      style={inputStyle}
-                    />
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      className="form__input"
-                      placeholder="Password"
-                      style={inputStyle}
-                    />
-                    <input
-                      type="password"
-                      name="ConfirmPassword"
-                      id="confirmpassword"
-                      className="form__input"
-                      placeholder="Confirm Password"
-                      style={inputStyle}
-                    />
-                  </div>
-                </div>
-  
-                <div>
-                  <input
-                    type="submit"
-                    value="Submit"
-                    className="btn"
-                    style={submitButtonStyle}
-                  />
-                </div>
-              </div>
-              <div>
-                <p>
-                  Do you already have an account? <Link to="/">Log In</Link>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
+        </form>
     );
-  }
-  
-  const mainContentStyle = {
-    width: "60%",
-    borderRadius: "20px",
-    boxShadow: "10px 10px 10px 10px rgba(0, 0, 0, 0.4)",
-    margin: "5em auto",
-    display: "flex",
-  };
-  
-  const companyInfoStyle = {
-    backgroundColor: "#008080",
-    borderTopLeftRadius: "20px",
-    borderBottomLeftRadius: "20px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    color: "#fff",
-    padding: "3em",
-  };
-  
-  const loginFormStyle = {
-    backgroundColor: "#fff",
-    width: "100%",
-    marginLeft: "2em",
-  };
-  
-  const inputStyle = {
-    width: "75%",
-    border: "0px solid transparent",
-    borderRadius: "0",
-    borderBottom: "1px solid #aaa",
-    padding: "1em 0.5em 0.5em",
-    paddingLeft: "2em",
-    outline: "none",
-    margin: "15px auto",
-    transition: "all 0.5s ease",
-  };
-  
-  const submitButtonStyle = {
-    transition: "all 0.5s ease",
-  width: "70%",
-  borderRadius: "30px",
-  color: "#008080",
-  fontWeight: "600",
-  backgroundColor: "#fff",
-  border: "1px solid #008080",
-  marginTop: "1.5em",
-  marginBottom: "1em",
-  };
-  
-  export default SignupPage;
+}
+
+export default SignupPage;
